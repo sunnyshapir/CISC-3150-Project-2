e01d1eca089e1696aef773985f174db6b3bd41c4
sunnyshapir <sunnyshapir@gmail.com>
2016-05-21 19:20:05 -0400

    StdDraw.java

--- StdDraw.java
+++ StdDraw.java
@@ -8,7 +8,6 @@ import java.awt.Image;
 import java.awt.MediaTracker;
 import java.awt.RenderingHints;
 import java.awt.Toolkit;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -16,28 +15,21 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 import java.awt.geom.Arc2D;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
-
 import java.awt.image.BufferedImage;
 import java.awt.image.DirectColorModel;
 import java.awt.image.WritableRaster;
-
 import java.io.File;
 import java.io.IOException;
-
 import java.net.URL;
-
 import java.util.LinkedList;
 import java.util.TreeSet;
 import java.util.NoSuchElementException;
-
 import javax.imageio.ImageIO;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
